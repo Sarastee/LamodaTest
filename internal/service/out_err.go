dfa8033ce2807fda5a2683a7308786b797284010
Ilya Lyakhov <ilja.sarasti@mail.ru>
2024-03-20 23:06:58 +0500

    custom errors little fix

--- internal/service/out_err.go
+++ internal/service/out_err.go
@@ -3,10 +3,9 @@ package service
 import "errors"
 
 const (
-	errWarehouseNotExists                       = "warehouse not exists"
-	errWarehouseNoAvailableWarehouses           = "there are no available warehouses"
-	errNotEnoughProductsOnWarehouse             = "not enough products on warehouses"
-	errNoWarehousesWithRequiredAmountOfProducts = "there are not warehouses with required amount of products"
+	errWarehouseNotExists             = "warehouse not exists"
+	errWarehouseNoAvailableWarehouses = "there are no available warehouses"
+	errNotEnoughProductsOnWarehouse   = "not enough products on warehouses"
 
 	errNotEnoughProductsInReserveList  = "not enough products on the reserve list"
 	errNoProductsWithCodeInReserveList = "there are no items with this code on the reserve list"
@@ -15,6 +14,5 @@ const (
 var ErrMsgWarehouseNotExists = errors.New(errWarehouseNotExists)
 var ErrMsgWarehouseNoAvailableWarehouses = errors.New(errWarehouseNoAvailableWarehouses)
 var ErrMsgNotEnoughProductsOnWarehouse = errors.New(errNotEnoughProductsOnWarehouse)
-var ErrMsgNoWarehousesWithRequiredAmountOfProducts = errors.New(errNoWarehousesWithRequiredAmountOfProducts)
 var ErrNotEnoughProductsInReserveList = errors.New(errNotEnoughProductsInReserveList)
 var ErrNoProductsWithCodeInReserveList = errors.New(errNoProductsWithCodeInReserveList)
